a70b08aab0b102e85e1d475a9639c37f5b35d174
Carlos Eduardo Corado <ccoradoi1@miumg.edu.gt>
2026-03-07 09:35:03 -0600

    Creación de menú y métodos para cada problema

--- src/Problemas/Main.java
+++ src/Problemas/Main.java
@@ -4,7 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
+public static void mostrarMenu() {
+	System.out.println("===== MENU PRIMER PARCIAL=====");
+    System.out.println("1. Problema A");
+    System.out.println("2. Problema B");
+    System.out.println("3. Problema C");
+    System.out.println("4. Problema D");
+    System.out.println("0. Salir");	
+	}
+public static void probblemaA() {
+	
+}
+public static void problemaB() {
+	
+}
+public static void problemaC() {
+	
+}
+public static void problemaD() {
+	
+}
 }
